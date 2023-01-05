b70abefc084546875db9ec3d10f3b3b3d9343c05
amedeo pasanisi <amedeopasanisi97@gmail.com>
2023-01-05 20:28:08 +0100

    debug ui

--- src/script.js
+++ src/script.js
@@ -1,14 +1,41 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'lil-gui'
 
 /**
  * Base
  */
+const parameters = {
+    color: 0xff0000,
+    spin: () =>
+    {
+        gsap.to(mesh.rotation, 1, { y: mesh.rotation.y + Math.PI * 2 })
+    }
+}
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+// const mesh = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+//     new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// )
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -23,23 +50,16 @@ window.addEventListener('resize', () =>
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
+    
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
-
-// Camera
+/**
+ * Camera
+*/
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.x = 2
 camera.position.y = 2
@@ -47,26 +67,66 @@ camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Debug
+ */
+const gui = new dat.GUI({
+    closed: true,
+    width: 400
+})
+//gui.add(mesh.position, 'y', - 3, 3, 0.01)
+gui
+    .add(mesh.position, 'y')
+    .min(- 3)
+    .max(3)
+    .step(0.01)
+    .name('y position')
+
+gui
+    .add(material, 'wireframe')
+
+gui
+    .addColor(parameters, 'color')
+    .onChange(() =>
+    {
+        material.color.set(parameters.color)
+    })
+
+gui.add(parameters, 'spin')
+
+window.addEventListener('keydown', (event) =>
+{
+    if(event.key === 'h')
+    {
+        if(gui._hidden)
+            gui.show()
+        else
+            gui.hide()
+    }
+})
+
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
 
     // Update controls
     controls.update()
